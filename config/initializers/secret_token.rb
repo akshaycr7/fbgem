5316d22a547d2e29910e978fbaa4e58f96ab1fe5
Akshay Thadani <akshay.thadani@newtechfusion.com>
2014-06-26 11:23:35 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbgem::Application.config.secret_token = '2383c1ce4741361584465423d1ea504a7d492a688ec44946e98cae24e59140a09eb4b798aa837d88cdf6b363bd9cd887d6381609b669bb751da5928c45ccc676'
